a77d1c67c7aa110733de49b61018117f6871a8b6
VoltXd <47433478+VoltXd@users.noreply.github.com>
2023-02-01 10:48:13 +0100

    Serveur
    - Ajout d'un message de démarrage du serveur
    - Meilleur routine de connexion des joueurs (les joueurs ont 60 secondes pour se connecter, le timeout est mieux géré)

--- Server/PyrudoServer.py
+++ Server/PyrudoServer.py
@@ -1,8 +1,9 @@
 import socket
 import PyrudoReferee
 import time
+import os
 
-CONNECTION_TIMEOUT = 10
+CONNECTION_TIMEOUT = 60
 MAX_PLAYERS = 6
 
 number_of_players = 0
@@ -11,7 +12,6 @@ players = []
 # Opening a socket
 socket_address = ("", 8081)
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.settimeout(10)
 server_socket.bind(socket_address)
 server_socket.listen()
 
@@ -20,15 +20,28 @@ end = time.time()
 
 elapsed_time = end - start
 
+if os.name == "nt":
+    os.system("cls")
+elif os.name == "posix":
+    os.system("clear")
+else:
+    print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+    
+print("******** Serveur Pyrudo ********")
+print("En attente d'une première connexion...")
+
 while (elapsed_time < CONNECTION_TIMEOUT and number_of_players < MAX_PLAYERS):
     try:
         client_socket, adrr = server_socket.accept()
         players.append(client_socket)
+        if number_of_players == 0:
+            start = time.time()
         number_of_players += 1
     except socket.timeout:
         pass
     end = time.time()
     elapsed_time = end - start
+    server_socket.settimeout(CONNECTION_TIMEOUT - elapsed_time)
     waiting_message = "En attente de connexion... Actuellement {} joueur(s) connecté(s)... {} seconde(s) restante(s)".format(
         len(players), int(CONNECTION_TIMEOUT-elapsed_time))
     for client in players:
